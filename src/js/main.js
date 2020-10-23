8e775c163b5ac6a83eb8a1b8c9980b43fbbb81c3
Mora Porthe <moraporthe@gmail.com>
2020-10-23 14:12:52 +0200

    add paint series function

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,34 @@
 "use strict";
 
-let seriesList = {};
+let seriesList = [];
+const searchResults = document.querySelector(".js-results");
+
+// Pedir series al servidor
 
 function getDataFromApi() {
   const inputValue = document.querySelector(".js-inputValue").value;
-  fetch("http://api.tvmaze.com/search/shows?q=" + inputValue)
+  fetch("//api.tvmaze.com/search/shows?q=" + inputValue)
     .then((response) => response.json())
     .then((data) => {
       seriesList = data;
       console.log(seriesList);
+      paintSeries();
     });
 }
+
+// pintar series
+
+const paintSeries = function () {
+  let codeHTML = "";
+  for (const show of seriesList) {
+    codeHTML += `<li>`;
+    codeHTML += `<img src=${show.show.image.medium} class="serie-img" alt="${show.show.name}">`;
+    codeHTML += `<h3 class="serie_title">${show.show.name}</h3>`;
+    codeHTML += `<button class="js-add-fav-btn" id="">Add to favorites</button>`;
+    codeHTML += `</li>`;
+  }
+  searchResults.innerHTML = codeHTML;
+};
 
 const btn = document.querySelector(".js-btn-Search");
 btn.addEventListener("click", getDataFromApi);
