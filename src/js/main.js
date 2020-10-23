1575e58c3a15c9bab8ec11ce5541997ca4cbb901
Mora Porthe <moraporthe@gmail.com>
2020-10-23 10:52:04 +0200

    get data from API

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let seriesList = {};
+
+function getDataFromApi() {
+  const inputValue = document.querySelector(".js-inputValue").value;
+  fetch("http://api.tvmaze.com/search/shows?q=" + inputValue)
+    .then((response) => response.json())
+    .then((data) => {
+      seriesList = data;
+      console.log(seriesList);
+    });
+}
+
+const btn = document.querySelector(".js-btn-Search");
+btn.addEventListener("click", getDataFromApi);
