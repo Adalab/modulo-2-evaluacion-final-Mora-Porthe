ba00a76d9dc44ed321103ed7b14a4ad1d1d8ce88
Mora Porthe <moraporthe@gmail.com>
2020-10-24 20:17:53 +0200

    fixed code before paint favorites

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,6 @@ function getDataFromApi() {
     .then((response) => response.json())
     .then((data) => {
       seriesList = data;
-      console.log(data);
       paintSeries();
     });
 }
@@ -44,24 +43,7 @@ const paintSeries = function () {
   lisenAddFavoritesBtns();
 };
 
-// Agregar a favoritos
-
-const addFavorites = (ev) => {
-  const clickedBtn = parseInt(ev.target.dataset.id);
-  const indexFav = favorites.indexOf(clickedBtn);
-  const isFavorite = indexFav !== -1;
-  if (isFavorite === false) {
-    favorites.push(clickedBtn);
-  } else {
-    favorites.splice(indexFav, 1);
-  }
-  console.log(favorites);
-};
-
-// Escuchar eventos
-
-const btn = document.querySelector(".js-btn-Search");
-btn.addEventListener("click", getDataFromApi);
+// Escuchar boton favoritos
 
 const lisenAddFavoritesBtns = () => {
   const btnsFavorites = document.querySelectorAll(".js-add-fav-btn");
@@ -69,3 +51,50 @@ const lisenAddFavoritesBtns = () => {
     btnFavorites.addEventListener("click", addFavorites);
   }
 };
+
+// Agregar a favoritos
+
+const addFavorites = (ev) => {
+  // obtengo el ID del producto clickeado
+  const clickedBtn = parseInt(ev.target.dataset.id);
+  //  obtengo el indice del elemento clickeado
+  const indexFav = favorites.indexOf(clickedBtn);
+  //  determino una operacion logica que retorna true o false
+  const isFavorite = indexFav !== -1;
+  if (isFavorite === false) {
+    favorites.push(clickedBtn);
+  } else {
+    favorites.splice(indexFav, 1);
+  }
+};
+
+// pintar favoritos
+
+const paintFavorites = function () {
+  let codeHTML = "";
+  for (const favorite of favorites) {
+    const indexFav = favorites.indexOf(clickedBtn);
+    const isFavorite = indexFav !== -1;
+
+    if (show.show.image !== null) {
+      codeHTML += `<li>`;
+      codeHTML += `<img src="${show.show.image.medium}" class="serie-img" alt="${show.show.name}"/>`;
+      codeHTML += `<h3 class="serie_title">${show.show.name}</h3>`;
+      codeHTML += `<button class="js-take-fav-btn" data-id="${show.show.id}">X</button>`;
+      codeHTML += `</li>`;
+    } else {
+      codeHTML += `<li>`;
+      codeHTML += `<img src="${imgDefault}" class="serie-img" alt="${show.show.name}"/>`;
+      codeHTML += `<h3 class="serie_title">${show.show.name}</h3>`;
+      codeHTML += `<button class="js-take-fav-btn" data-id="${show.show.id}">X</button>`;
+      codeHTML += `</li>`;
+    }
+    const favoriteList = document.querySelector(".ja-favorites");
+    favoriteList.innerHTML = codeHTML;
+  }
+};
+
+// Escuchar eventos
+
+const btn = document.querySelector(".js-btn-Search");
+btn.addEventListener("click", getDataFromApi);
